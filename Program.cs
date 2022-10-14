c4df2629d2de21b3143a32cead0773f00879bed2
Andreas <andreas.kuylenstierna@gmail.com>
2022-10-14 14:09:28 +0200

    Testkod...

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@
             Console.WriteLine("Välkommen till frågesport. Nu ska vi testa ditt kunnande!");
             Console.WriteLine("Testing!");
             Lycka();
+            Console.ReadKey();
         }
     }
 }
