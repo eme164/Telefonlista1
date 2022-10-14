a624358f83a955c62ffccdf3841a01e816a231fb
Andreas <andreas.kuylenstierna@gmail.com>
2022-10-14 13:36:59 +0200

    Rubrik

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--- Frågesport V 0.01 --- "); //Andreas har kluddrat lite...
             Console.WriteLine("Välkommen till frågesport. Nu ska vi testa ditt kunnande!");
         }
     }
