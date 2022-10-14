1e2b5f503c2ec3dba8b83b9e77ab75b43f357d62
emil <emil.o.jonsson@gmail.com>
2022-10-14 13:26:02 +0200

    Lagt till rubrik

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Välkommen till frågesport. Nu ska vi testa ditt kunnande!");
         }
     }
 }
